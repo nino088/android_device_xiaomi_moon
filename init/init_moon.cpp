a696c42ac774b736f25bcc5867ddd0da3088e44e
nino <pilopilosimon1990@gmail.com>
2026-03-05 20:49:52 +0100

    Update and rename init_pond.cpp to init_moon.cpp

--- init/init_moon.cpp
+++ init/init_moon.cpp
@@ -53,19 +53,6 @@ void model_property_override(const std::string& device, const std::string& model
 void vendor_load_properties() {
     const std::string sku = GetProperty("ro.boot.product.hardware.sku", "");
 
-    if (sku == "c3na") {
-        model_property_override("pond", "Redmi 14C");
-    } else if (sku == "c3ncn") {
-        model_property_override("pond", "Redmi 14C");
-    } else if (sku == "c3nl") {
-        model_property_override("pond", "Redmi 14C");
-    } else if (sku == "c3nn") {
-        model_property_override("lake", "Redmi 14C");
-    } else if (sku == "c3npg") {
-        model_property_override("lake", "Poco C75");
-    } else if (sku == "c3yn") {
-        model_property_override("pond", "Redmi A3 Pro");
-    } else {
-        model_property_override("lake", "Redmi 14C");
-    }
+    if (sku == "24040RN64Y") {
+        model_property_override("moon", "Redmi 13");
 }
